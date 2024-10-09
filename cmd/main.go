165868bccc8b8a20d0074d0e4f683a0a10e85e29
Julie <juliekivuva@gmail.com>
2024-10-10 00:08:28 +0300

    changing vm key paths

--- cmd/main.go
+++ cmd/main.go
@@ -13,19 +13,16 @@ import (
 )
 
 func main() {
-	// Connect to MySQL
 	dsn := "user:password@tcp(mysql-service:3306)/testdb?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 
-	// Auto migrate the database schema
 	if err := model.Migrate(db); err != nil {
 		log.Fatalf("failed to migrate database: %v", err)
 	}
 
-	// Set up gRPC server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
