edfe77dacbdd76f0409337bf0021cbbec7eed3e6
Jaycynth <juliekivuva@gmail.com>
2024-10-26 23:46:45 +0300

    terraform fix

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,22 @@
-provider "aws" {
-  region = "eu-west-1"
- 
+terraform {
+  required_providers {
+    aws={
+        source = "hashicorp/aws"
+        version = "5.32.1"
+    }
+  }
 }
 
 
+provider "aws" {
+  region = "eu-west-1"
+  access_key  = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+ 
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_access_key" {}
 
 # Define variables for instance settings
 variable "instance_type" {
